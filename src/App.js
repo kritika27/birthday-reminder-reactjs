b2483fde67fb13d93e485759eb3be51ebe505104
kritika <kritikasri27@gmail.com>
2021-01-30 16:36:49 +0530

    Added file containing people' details and the logic to display birthdays in current month

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data";
+import "./App.css";
 
-function App() {
+export default function App() {
+  var d = new Date();
+  var n = d.getMonth();
+  switch (n) {
+    case 0:
+      var month = "January";
+      break;
+    case 1:
+      month = "February";
+      break;
+    case 2:
+      month = "March";
+      break;
+    case 3:
+      month = "April";
+      break;
+    case 4:
+      month = "May";
+      break;
+    case 5:
+      month = "June";
+      break;
+    case 6:
+      month = "July";
+      break;
+    case 7:
+      month = "August";
+      break;
+    case 8:
+      month = "September";
+      break;
+    case 9:
+      month = "October";
+      break;
+    case 10:
+      month = "November";
+      break;
+    case 11:
+      month = "December";
+      break;
+    default:
+      return 0;
+  }
+
+  data.sort(function (a, b) {
+    return a.day - b.day;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="birthday">
+      <h1>Birthdays Of The Month</h1>
+      <div className="container">
+        {data
+          .filter((bday) => bday.month === month.toLowerCase())
+          .map((people) => {
+            return (
+              <div className="person" key={people.id}>
+                <img src={people.image} alt="person" />
+                <div className="info">
+                  <h3>{people.name}</h3>
+                  <h4>
+                    {people.day} {month}
+                  </h4>
+                </div>
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,43 @@
+const data = [
+  {
+    id: 1,
+    name: "Bertie Yates",
+    day: 29,
+    month: "january",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    day: 15,
+    month: "january",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    day: 28,
+    month: "march",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    day: 30,
+    month: "january",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    day: 29,
+    month: "may",
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+  },
+];
+export default data;
